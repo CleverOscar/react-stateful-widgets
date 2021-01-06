edc48656608da1d8f7f36b624cb2603fac8f8042
Oscar Pacheco <Oscarpa88@yahoo.com>
2021-01-06 12:49:27 -0600

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,18 +3,6 @@
 Watch this short video carefully, paying attention to the UI and Chrome Devtools:
 https://tk-assets.lambdaschool.com/59036a85-0980-42c8-81ad-9afc8354497f_counter-clip.gif
 
-STEP 4:
-  This click handler needs to use 'setCount' to schedule the 'count' to become the current 'count' plus one.
-  These state changes are not synchronous: the updated count arrives on the next run of the Counter component.
-  Do NOT simply do count++. The plus plus is forbidden! We never mutate a slice of state in place. Even if you could
-  reassign a const, React would not be aware anything changed. Always use the state updater, passing in a new value.
-
-STEP 5:
-  This click handler needs to use 'setCount' to set the 'count' to be the current 'count' minus one.
-  Do NOT do count--. That amounts to trying to mutate 'count' in place. This is the road to perdition.
-
-STEP 6:
-  This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
 import React, {useState} from 'react';
@@ -23,13 +11,13 @@ export default function Counter() {
 
   const [count, setCount] = useState(0);
   const increment = () => {
-    return setCount(count = useState(+1))
+    setCount(count + 1)
   };
   const decrement = () => {
-    /* STEP 5 */
+    setCount(count - 1);
   };
   const reset = () => {
-    /* STEP 6 */
+    setCount(0);
   };
 
   const style = {
